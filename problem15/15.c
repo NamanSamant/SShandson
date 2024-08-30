b162c2fb71ec08941e6122204012b829e131ba68
NamanSamant <samant.naman15@gmail.com>
2024-08-30 13:50:03 +0530

    added problem15

---
+++ problem15/15.c
@@ -0,0 +1,19 @@
+/*
+============================================================================
+Name : 15.c
+Author : Naman Samant
+Description : Write a program to display the environmental variable of the user (use environ).
+Date: 29th Aug, 2024.
+============================================================================
+*/
+
+#include <stdio.h>
+extern char **environ;
+int main() {
+	char **env = environ;
+	while(*env) {
+		printf("%s\n", *env);
+		env++;
+	}
+	return 0;
+}
