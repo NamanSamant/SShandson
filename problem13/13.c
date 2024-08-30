721d4009dbbed782659855df6d1d2191789758c8
NamanSamant <samant.naman15@gmail.com>
2024-08-30 12:20:36 +0530

    added problem13

---
+++ problem13/13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <string.h>
+int main(){
+	fd_set readFds;
+	struct timeval tOut;
+	tOut.tv_sec = 10;
+	tOut.tv_usec = 0;
+	FD_ZERO(&readFds);
+	FD_SET(STDIN_FILENO, &readFds);
+	printf("Waiting for input(for 10 seconds)\n");
+	int value = select(STDIN_FILENO + 1, &readFds, NULL, NULL, &tOut);
+	if(value == -1) {
+		printf("There's an error while reading input\n");
+		return 0;
+	}
+	if(value == 0) {
+		printf("No Data was entered in 10 seconds\n");
+		return 0;
+	}
+	if(FD_ISSET(STDIN_FILENO, &readFds)) {
+		char buffer[1024];
+		printf("Data available on STDIN\n");
+		ssize_t byteR = read(STDIN_FILENO, buffer, 1024);
+		buffer[byteR] = '\0';
+		printf("Data = %s\n", buffer);
+	}
+	return 0;
+}
