df8c6d510adc747e7c630d03fc330ecc32834c7d
NamanSamant <samant.naman15@gmail.com>
2024-09-03 17:08:37 +0530

    made some changes

--- problem5/5.c
+++ problem5/5.c
@@ -15,16 +15,17 @@ int main()
 {
 	const char *fileNames[] = {"f1.txt", "f2.txt", "f3.txt", "f4.txt", "f5.txt"};
 	int fD[5];
-	for(int i=0;i<5;i++)
-	{
-		char fName[30];
-		strcpy(fName, fileNames[i]);
-		fD[i] = open(fName, O_CREAT | O_RDWR, 0644);
-		if(fD[i]==-1) printf("error creating the file%d\n",i);
-		else printf("Created %s with FD value = %d\n",fName, fD[i]);
-	}
+	int i=0;
 	while(1)
 	{
+	        for(i;i<5;i++)
+	        {
+		          char fName[30];
+		          strcpy(fName, fileNames[i]);
+		          fD[i] = open(fName, O_CREAT | O_RDWR, 0644);
+		          if(fD[i]==-1) printf("error creating the file%d\n",i);
+		          else printf("Created %s with FD value = %d\n",fName, fD[i]);
+	        }
 		sleep(1);
 	}
 	return 0;
