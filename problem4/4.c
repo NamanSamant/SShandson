1e785f02a535ad21e2835794c6a132aafef6d204
NamanSamant <samant.naman15@gmail.com>
2024-09-03 16:46:52 +0530

    corrected some programs

--- problem4/4.c
+++ problem4/4.c
@@ -9,14 +9,16 @@ Date: 29th Aug, 2024.
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<errno.h>
+#include<string.h>
 int main()
 {
 	int fD=open("open-file.txt", O_RDWR);
 	if(fD==-1) printf("There is an error while opening file using open() function\n");
 	else printf("Value of File Descriptor = %d\n",fD);
 	close(fD);
-	fD=open("open-file.txt", O_RDWR | O_CREAT | O_EXCL);
-	if(fD==-1) printf("There's an error while opening file with O_EXCL\n");
+	fD=open("open-file.txt", O_RDWR | O_CREAT | O_EXCL, 0644);
+	if(fD==-1) printf("There's an error while opening file with O_EXCL because - %s\n", strerror(errno));
 	else printf("Value of fD (O_EXCL) = %d\n", fD);
 	close(fD);
 	return 0; 
